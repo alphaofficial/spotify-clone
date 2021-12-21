3241330b480098d5bc25bcc3e516dfbb71b85fe7
Albert Akrong <88777176+albertakrong@users.noreply.github.com>
2021-12-21 19:22:05 +0000

    add token validation handler

---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./prisma";
+
+// protect API route
+export const validate = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    // check if token is valid
+    const { TRAX_ACCESS_TOKEN: token } = req.cookies;
+    if (token) {
+      let user;
+      try {
+        const { id } = jwt.verify(token, process.env.TRAX_JWT_SECRET) as {
+          id: number;
+        };
+        user = prisma.user.findUnique({ where: { id } });
+        if (!user) {
+          throw new Error("not real user");
+        }
+      } catch (error) {
+        res.status(401);
+        res.json({ error: "Not authorized" });
+        return;
+      }
+
+      return handler(req, res, user);
+    }
+    res.status(401);
+    res.json({ error: "Not authorized" });
+  };
+};
