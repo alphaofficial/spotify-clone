d767581a975abb45c3ff46c55367a123345b7d5b
Albert Akrong <88777176+albertakrong@users.noreply.github.com>
2021-12-25 12:27:11 +0000

    show artists

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,56 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import GradientLayout from "../components/gradientLayout";
 import prisma from "../lib/prisma";
 
 const Home = ({ artists }) => {
   return (
     <GradientLayout
-      color="green"
+      color="gray"
       subTitle="Profile"
       title="Albert Akrong"
       description="15 public playlist"
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
       roundImage
     >
-      <div>home page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="20px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists this month
+          </Text>
+          <Text fontSize="md">Only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box
+                key={artist.id}
+                bg="gray.900"
+                borderRadius="4px"
+                padding="15px"
+                width="100%"
+              >
+                <Image src="http://placekitten.com/300" borderRadius="100%" />
+                <Box marginTop="10px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">ARTIST</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany();
+  return {
+    props: {
+      artists,
+    },
+  };
 };
 
 export default Home;
